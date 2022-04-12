152dbe6176eb56139852654f4135ab2a0b94a703
mashaLutskevich2002 <masha.lutskevich2002@gmail.com>
2022-04-12 22:30:17 +0300

    add logo

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -4,7 +4,7 @@ export const Header: React.FC = () => {
     return (
         <nav>
             <div className="nav-wrapper deep-orange darken-4 pd-10">
-                <a href="#" className="brand-logo">Logo</a>
+            <a href="#" className="brand-logo">TodoList <i className='material-icons'> done</i></a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
                     <li><a href="/">My todo </a></li>
                     <li><a href="/">Information</a></li>
